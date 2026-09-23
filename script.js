c0d98867f566cb1667018f4e1d21d13d5234319f
yepesflorezadrianalucia <yepesflorezadrianalucia@gmail.com>
2026-09-23 18:34:25 -0500

    Actualizacion de Codgigo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
    ========================================================= */
 
 // CONFIGURACIÓN SUPABASE
-const SUPABASE_URL = "https://zqnjhqchnzqailpqlfvb.supabase.co";
+const SUPABASE_URL = "https://zqnjhqchnzqailpqlfvb.supabase.co"; 
 const SUPABASE_ANON_KEY = "sb_publishable_UteEe99FemnxY2udwsXUkw_nE61X1r7";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
